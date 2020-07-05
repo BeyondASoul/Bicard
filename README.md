ec50d4fe5954e76fa2f78423e3f76345c6cf8cfa
BeyondASoul <alejandroo.bryan@gmail.com>
2020-07-05 11:47:58 -0500

    Gif added

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 # Bicard
 
 Presentación de verbos y palabras sólo en tarjetas, disponible en iOS y Android.
+
+### Bicard UI
+![Preview](/demo.gif)
--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_swiper/flutter_swiper.dart';
-import 'package:flutter_page_indicator/flutter_page_indicator.dart';
 
 import 'detail_page.dart';
 import 'constants.dart';
